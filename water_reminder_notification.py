dc5b40d330f3d3e8ac67b7ccadfe891a28699561
Diya Jain <amitanitinjain59@gmail.com>
2025-12-27 22:30:55 +0530

    files organizer

--- water_reminder_notification.py
+++ water_reminder_notification.py
@@ -4,4 +4,4 @@ from plyer import notification
 while True :
     notification.notify(title = 'Drink water' , message = 'Have a glass of water')
     time.sleep(60*60)
-    # print("successful")
+    print("successful")
